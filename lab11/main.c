86d3e3ae15ea2f222973b2c4e2ff4847d515b6d2
vis4rd <alagaesia.vrael@gmail.com>
2021-05-22 19:37:54 +0200

    adjusted code in lab11 to work with plotting script

--- lab11/main.c
+++ lab11/main.c
@@ -15,30 +15,29 @@ int main(void)
 {
     srand(time(NULL));
     int N;
-    float T, tMax, deltaT, sigma, *f, *fstart, *g1, *g2;
+    float T, tMax, deltaT, sigma, *f, *g1, *g2;
     float ti;
     float a1, b1, a2, b2;
+    char *files[3] = {"k8.dat", "k10.dat", "k12.dat"};
 
-    FILE *file = fopen("result.txt", "w");
     for(int k = 8; k <= 12; k += 2)
     {
+        FILE *file = fopen(files[(k-8)/2], "w");
         N = pow(2, k);
         T = 1.0;
         tMax = 3*T;
         deltaT = tMax/N;
         sigma = T/20;
 
-        //printf("deltaT = %f\n", deltaT);
-
         f = vector(1, 2*N);
-        fstart = vector(1, 2*N);
+        //fstart = vector(1, 2*N);
         g1 = vector(1, 2*N);
         g2 = vector(1, 2*N);
 
         for(int i = 1; i <= N; i++)
         {
             f[i] = 0;
-            fstart[i] = 0.f;
+            //fstart[i] = 0.f;
             g1[i] = 0.f;
             g2[i] = 0.f;
         }
@@ -47,10 +46,11 @@ int main(void)
         {
             ti = deltaT * (i-1);
             f[2*i - 1] = fun(T, ti) + randD(-0.5, 0.5);
-            fstart[2*i - 1] = fun(T, ti) + randD(-0.5, 0.5);
+            //fstart[2*i - 1] = fun(T, ti) + randD(-0.5, 0.5);
 
             g1[2*i - 1] = gun(sigma, ti);
             g2[2*i - 1] = gun(sigma, ti);
+            fprintf(file, "%f %f\n", ti, f[2*i - 1]);
         }
 
         four1(f, N, 1);
@@ -70,24 +70,23 @@ int main(void)
         four1(f, N, -1);
 
         float maxval = fabs(f[1]);
-        for(int i = 2; i <= N; i++)
+        for(int i = 1; i <= N; i++)
             maxval = (fabs(maxval) > fabs(f[2*i - 1])) ? fabs(maxval) : fabs(f[2*i - 1]);
-        //printf("maxval = %f\n", maxval);
 
+
+        fprintf(file, "\n\n");
         for(int i = 1; i <= N; i++)
         {
             ti = deltaT * (i-1);
-            fprintf(file, "%f %f %f %f\n", ti, fun(T, ti), fstart[2*i - 1], 2.5f * f[2*i - 1] / maxval);
+            fprintf(file, "%f %f\n", ti-0.03f, f[2*i - 1]/maxval*2.5f);
         }
-        fprintf(file, "\n\n");
 
         free_vector(f, 1, 2*N);
-        free_vector(fstart, 1, 2*N);
+        //free_vector(fstart, 1, 2*N);
         free_vector(g1, 1, 2*N);
         free_vector(g2, 1, 2*N);
+        fclose(file);
     }
-
-    fclose(file);
     return 0;
 }
 
